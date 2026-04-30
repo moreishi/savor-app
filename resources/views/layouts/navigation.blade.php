eb67154fc25e4ec982394d9761efcd87daf46b74
moreishi <moreishi@gmail.com>
2026-04-30 22:46:54 +0800

    seed 50 Filipino recipes (up from 17)
    - Added: Bulanglang, Ensaladang Talong, Adobong Kangkong, Puto, Maja Blanca
    - Added: Inihaw na Pusit, Crispy Pata, Tokwa't Baboy
    - Navigation: Import Prices link added to mobile responsive nav
    - Full category coverage: meat(19), seafood(6), vegetables(8), noodles(5), desserts(7), soups(5)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('prices.import')" :active="request()->routeIs('prices.*')">
+                {{ __('Import Prices') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
